a869e18ec6708a781ba15f975a889ee5ac69b464
br1dp <brunoen99@gmail.com>
2026-02-08 10:50:33 -0300

    Mapeo de clases

--- src/main/java/com/programacion/gestioncursos/model/Curso.java
+++ src/main/java/com/programacion/gestioncursos/model/Curso.java
@@ -4,21 +4,24 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import java.time.LocalDate;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
 public class Curso {
-
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id_curso;
     private String nombre;
     private String modalidad;
     private LocalDate fecha_finalizacion;
+    @OneToMany(mappedBy="curso")
     private List<Tema> lista_temas;
 
 }
--- src/main/java/com/programacion/gestioncursos/model/Tema.java
+++ src/main/java/com/programacion/gestioncursos/model/Tema.java
@@ -1,14 +1,26 @@
 package com.programacion.gestioncursos.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
 public class Tema {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id_tema;
     private String nombre;
     private String descripcion;
+    @ManyToOne
+    @JoinColumn(name = "id_curso")
+    private Curso curso;
 
 }
